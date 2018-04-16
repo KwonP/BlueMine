f10dada7829492e0d06e1f6b5506daf84a4369a3
Kwonp <ckdrnjs513@gmail.com>
2018-04-16 16:49:08 +0900

    Add DAO and Mapper

---
+++ src/main/java/com/bluemine/www/dao/HorseHeadDAO.java
@@ -0,0 +1,41 @@
+package com.bluemine.www.dao;
+
+import java.util.ArrayList;
+
+import javax.inject.Inject;
+
+import org.apache.ibatis.session.SqlSession;
+
+import com.bluemine.www.interfaces.horseHeadMapper;
+import com.bluemine.www.vo.HorseHead;
+
+public class HorseHeadDAO implements horseHeadMapper{
+
+	@Inject
+	SqlSession session;
+	
+	//말머리 추가
+	@Override
+	public int addHorseHead(HorseHead hoHead) {
+		int result=0;
+		result = session.getMapper(horseHeadMapper.class).addHorseHead(hoHead);
+		return result;
+	}
+
+	//전체말머리 가져오기
+	@Override
+	public ArrayList<HorseHead> getAll() {
+		ArrayList<HorseHead> list = new ArrayList<>();
+		list = session.getMapper(horseHeadMapper.class).getAll();
+		return list;
+	}
+
+	//한종류의 말머리 가져오기
+	@Override
+	public ArrayList<HorseHead> getOneType(int tagType) {
+		ArrayList<HorseHead> list = new ArrayList<>();
+		list = session.getMapper(horseHeadMapper.class).getOneType(tagType);
+		return list;
+	}
+
+}
--- src/main/java/com/bluemine/www/interfaces/horseHeadMapper.java
+++ src/main/java/com/bluemine/www/interfaces/horseHeadMapper.java
@@ -7,7 +7,7 @@ import com.bluemine.www.vo.HorseHead;
 public interface horseHeadMapper {
 
 	//말머리 추가
-	public int addHorseHead();
+	public int addHorseHead(HorseHead hoHead);
 	//전체말머리 가져오기
 	public ArrayList<HorseHead> getAll();
 	//한종류의 말머리 가져오기
